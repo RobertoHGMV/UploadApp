58d013e896c390e3664d790004cf10de2fe8fd6f
robertohgmv <robertohgm@hotmail.com>
2018-01-15 22:42:30 -0200

    first commit

---
+++ UploadApp/UploadForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace UploadApp
+{
+    partial class UploadForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.lbServer = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.lbUser = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lbFile = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(393, 169);
+            this.btnUpload.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(112, 35);
+            this.btnUpload.TabIndex = 6;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // lbServer
+            // 
+            this.lbServer.AutoSize = true;
+            this.lbServer.Location = new System.Drawing.Point(25, 29);
+            this.lbServer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbServer.Name = "lbServer";
+            this.lbServer.Size = new System.Drawing.Size(71, 20);
+            this.lbServer.TabIndex = 13;
+            this.lbServer.Text = "Servidor:";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(109, 214);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(521, 35);
+            this.progressBar.TabIndex = 12;
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(313, 254);
+            this.lbStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(112, 20);
+            this.lbStatus.TabIndex = 11;
+            this.lbStatus.Text = "Progresso: 0%";
+            this.lbStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbUser
+            // 
+            this.lbUser.AutoSize = true;
+            this.lbUser.Location = new System.Drawing.Point(25, 62);
+            this.lbUser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbUser.Name = "lbUser";
+            this.lbUser.Size = new System.Drawing.Size(68, 20);
+            this.lbUser.TabIndex = 9;
+            this.lbUser.Text = "Usuário:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUserName.Location = new System.Drawing.Point(109, 59);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(521, 26);
+            this.txtUserName.TabIndex = 2;
+            // 
+            // txtServer
+            // 
+            this.txtServer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtServer.Location = new System.Drawing.Point(109, 26);
+            this.txtServer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(521, 26);
+            this.txtServer.TabIndex = 1;
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(25, 95);
+            this.lbPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(60, 20);
+            this.lbPassword.TabIndex = 18;
+            this.lbPassword.Text = "Senha:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPassword.Location = new System.Drawing.Point(109, 92);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(521, 26);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // lbFile
+            // 
+            this.lbFile.AutoSize = true;
+            this.lbFile.Location = new System.Drawing.Point(25, 128);
+            this.lbFile.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbFile.Name = "lbFile";
+            this.lbFile.Size = new System.Drawing.Size(62, 20);
+            this.lbFile.TabIndex = 20;
+            this.lbFile.Text = "Arquivo";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFileName.Location = new System.Drawing.Point(109, 125);
+            this.txtFileName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.ReadOnly = true;
+            this.txtFileName.Size = new System.Drawing.Size(521, 26);
+            this.txtFileName.TabIndex = 4;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(217, 169);
+            this.btnSelect.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(168, 35);
+            this.btnSelect.TabIndex = 5;
+            this.btnSelect.Text = "Selecionar Arquivo";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // UploadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 295);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.lbFile);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.lbServer);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.lbUser);
+            this.Controls.Add(this.txtUserName);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(663, 301);
+            this.Name = "UploadForm";
+            this.Text = "UploadForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Label lbServer;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.Label lbUser;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lbFile;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnSelect;
+    }
+}
---
+++ UploadApp/UploadForm.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UploadApp
+{
+    public partial class UploadForm : Form
+    {
+        public UploadForm()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        struct FtpSetting
+        {
+            public string Server { get; set; }
+
+            public string UserName { get; set; }
+
+            public string Password { get; set; }
+
+            public string FileName { get; set; }
+
+            public string FullName { get; set; }
+        }
+
+        FtpSetting _inputParameter;
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                var fileName = ((FtpSetting)e.Argument).FileName;
+                var fullName = ((FtpSetting)e.Argument).FullName;
+                var userName = ((FtpSetting)e.Argument).UserName;
+                var password = ((FtpSetting)e.Argument).Password;
+                var server = ((FtpSetting)e.Argument).Server;
+
+                var request = (FtpWebRequest)WebRequest.Create(new Uri($"{server}/{fileName}"));
+                request.Method = WebRequestMethods.Ftp.UploadFile;
+                request.Credentials = new NetworkCredential(userName, password);
+
+                var ftpStream = request.GetRequestStream();
+                var fileStream = File.OpenRead(fullName);
+                var buffer = new byte[1024];
+                var total = (double)fileStream.Length;
+                int byteRead = 0;
+                double read = 0;
+
+                do
+                {
+                    if (!backgroundWorker1.CancellationPending)
+                    {
+                        byteRead = fileStream.Read(buffer, 0, 1024);
+                        ftpStream.Write(buffer, 0, byteRead);
+                        read += byteRead;
+                        var percentage = read / total * 100;
+                        backgroundWorker1.ReportProgress((int)percentage);
+                    }
+                }
+                while (byteRead != 0);
+                fileStream.Close();
+                ftpStream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Mensagem do Sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            lbStatus.Text = $"Progresso: {e.ProgressPercentage}%";
+            progressBar.Value = e.ProgressPercentage;
+            progressBar.Update();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lbStatus.Text = "Upload completo!";
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ValidateFields();
+
+                var fileInfo = new FileInfo(txtFileName.Text);
+                _inputParameter.UserName = txtUserName.Text;
+                _inputParameter.Password = txtPassword.Text;
+                _inputParameter.Server = txtServer.Text;
+                _inputParameter.FileName = fileInfo.Name;
+                _inputParameter.FullName = fileInfo.FullName;
+
+                backgroundWorker1.RunWorkerAsync(_inputParameter);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Mensagem do Sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var fileDialog = new OpenFileDialog { Multiselect = false, ValidateNames = true, Filter = "Todos os Arquivos|*.*" })
+                {
+                    if (fileDialog.ShowDialog() == DialogResult.OK)
+                        txtFileName.Text = fileDialog.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Mensagem do Sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ValidateFields()
+        {
+            if (string.IsNullOrEmpty(txtServer.Text))
+                throw new Exception("Informe o servidor");
+
+            if (string.IsNullOrEmpty(txtUserName.Text))
+                throw new Exception("Informe o usuário");
+
+            if (string.IsNullOrEmpty(txtPassword.Text))
+                throw new Exception("Informe a senha");
+
+            if (string.IsNullOrEmpty(txtFileName.Text))
+                throw new Exception("Selecione um arquivo");
+        }
+    }
+}
